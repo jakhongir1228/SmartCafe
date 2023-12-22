409600a503a5d44932f0e662ea3ea90260a52383
jakhongir1228 <62322793+jakhongir1228@users.noreply.github.com>
2023-12-22 14:52:05 +0500

    Update build.gradle.kts

--- feature/mvi/build.gradle.kts
+++ feature/mvi/build.gradle.kts
@@ -20,7 +20,6 @@ dependencies {
     implementation("com.badoo.reaktive:reaktive:2.0.1")
     implementation("com.badoo.reaktive:reaktive-annotations:2.0.1")
     implementation("com.badoo.reaktive:coroutines-interop:2.0.1")
-    implementation("com.badoo.reaktive:rxjava2-interop:2.0.1")
     implementation("com.badoo.reaktive:rxjava3-interop:2.0.1")
 
     implementation(project(":feature:kittens"))
